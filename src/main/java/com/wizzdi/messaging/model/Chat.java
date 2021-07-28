73c5fe92eb0055e94fa984c4de65b62c36fb948b
Asaf Ben Natan <asaf@wizzdi.com>
2021-07-28 11:14:27 +0300

    added oneToMany on chat->ChatToChatUser

--- src/main/java/com/wizzdi/messaging/model/Chat.java
+++ src/main/java/com/wizzdi/messaging/model/Chat.java
@@ -1,10 +1,14 @@
 package com.wizzdi.messaging.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.flexicore.model.Baseclass;
 import com.flexicore.model.Basic;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Chat extends Basic {
@@ -12,6 +16,9 @@ public class Chat extends Basic {
 	private Baseclass security;
 	@ManyToOne(targetEntity = ChatUser.class)
 	private ChatUser owner;
+	@JsonIgnore
+	@OneToMany(targetEntity = ChatToChatUser.class,mappedBy = "chat")
+	private List<ChatToChatUser> chatToChatUsers=new ArrayList<>();
 
 
 	@ManyToOne(targetEntity = Baseclass.class)
@@ -31,6 +38,17 @@ public class Chat extends Basic {
 
 	public <T extends Chat> T setOwner(ChatUser participant) {
 		this.owner = participant;
+		return (T) this;
+	}
+
+	@JsonIgnore
+	@OneToMany(targetEntity = ChatToChatUser.class,mappedBy = "chat")
+	public List<ChatToChatUser> getChatToChatUsers() {
+		return chatToChatUsers;
+	}
+
+	public <T extends Chat> T setChatToChatUsers(List<ChatToChatUser> chatToChatUsers) {
+		this.chatToChatUsers = chatToChatUsers;
 		return (T) this;
 	}
 }
